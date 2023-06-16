f6fce956d61572831e743e170707fb4b6febaf42
thatmattlove <matt@stellar.tech>
2023-06-15 22:31:43 -0400

    restructure response types

--- types/general.go
+++ types/general.go
@@ -15,23 +15,6 @@ type Attributes struct {
 	URL  string `json:"url"`
 }
 
-type RecordResponse[R any] struct {
-	Done      bool `json:"done"`
-	TotalSize int  `json:"totalSize"`
-	Records   []R  `json:"records"`
-}
-
-type GenericResponse struct {
-	Message string         `json:"message"`
-	Data    map[string]any `json:"data,omitempty"`
-}
-
-type RecordCreatedResponse struct {
-	Errors  []string `json:"errors"`
-	ID      string   `json:"id"`
-	Success bool     `json:"success"`
-}
-
 type BaseObject struct {
 	Attributes       Attributes `json:"attributes"`
 	CreatedByID      string     `json:"CreatedById"`
--- types/requests.go
+++ types/requests.go
@@ -3,3 +3,20 @@ package types
 type CachedTokenCallback func() (string, error)
 
 type SetTokenCallback func(token string, expiresIn float64) error
+
+type RecordCreatedResponse struct {
+	Errors  []string `json:"errors"`
+	ID      string   `json:"id"`
+	Success bool     `json:"success"`
+}
+
+type RecordResponse[R any] struct {
+	Done      bool `json:"done"`
+	TotalSize int  `json:"totalSize"`
+	Records   []R  `json:"records"`
+}
+
+type GenericResponse struct {
+	Message string         `json:"message"`
+	Data    map[string]any `json:"data,omitempty"`
+}
